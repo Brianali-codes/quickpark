c62f7640a0d37158ceee230d1bd28677707a7a45
Brian Ali <gamerfunky78@gmail.com>
2025-02-04 16:08:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 let popup = document.getElementById('popup')
+
+function togglePopup(){
+    if (popup.style.display === 'flex'){
+        popup.style.display = 'none'
+    }
+    else{
+        popup.style.display = 'flex'
+    }
+}
+
+
+
